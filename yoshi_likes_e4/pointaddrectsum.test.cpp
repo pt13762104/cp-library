c82edfc77647be3b7328de9d969ebde7cc81617f
Yoshi_likes_e4 <104140648+pt13762104@users.noreply.github.com>
2024-12-17 07:29:44 +0700

    Much faster Point Add Rectangle Sum implementation

--- yoshi_likes_e4/pointaddrectsum.test.cpp
+++ yoshi_likes_e4/pointaddrectsum.test.cpp
@@ -1,81 +1,14 @@
-// @brief Point Add Rectangle Sum
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_rectangle_sum"
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
 using namespace __gnu_pbds;
-#define int uint32_t
 #ifndef yoshi_likes_e4
 #define endl '\n'
 #endif
 #define multitest 0
 #define debug(x) cerr << #x << " = " << x << endl;
 const int FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-struct custom_hash_table
-{
-    int LG = 0, count = 0;
-    size_t hash(uint32_t x)
-    {
-        // shifting by 64 bits is undefined
-        return (LG ? ((x + FIXED_RANDOM) * 11995408973635179863ULL) >> (64 - LG) : 0);
-    }
-    vector<vector<pair<uint32_t, uint64_t>>> buckets = {{}};
-    void rehash()
-    {
-        auto old_buckets = buckets;
-        LG += 3;
-        buckets.resize(1 << LG);
-        for (int i = 0; i < old_buckets.size(); i++)
-            buckets[i] = {};
-        for (auto &i : old_buckets)
-            for (auto &j : i)
-                buckets[hash(j.first)].push_back(j);
-    }
-    void add(uint32_t k, uint32_t v)
-    {
-        size_t hs = hash(k);
-        for (auto &i : buckets[hs])
-            if (i.first == k)
-            {
-                i.second += v;
-                return;
-            }
-        buckets[hs].push_back({k, v});
-        count++;
-        if (count > 7 * (1 << LG))
-            rehash();
-    };
-    uint64_t query(uint32_t k)
-    {
-        size_t hs = hash(k);
-        for (auto &i : buckets[hs])
-            if (i.first == k)
-                return i.second;
-        return 0ul;
-    };
-};
-custom_hash_table fw_tree[200001];
-int MAX = 200000;
-uint64_t getpfs(int i, int j)
-{
-    int k = j;
-    uint64_t res = 0;
-    for (; i; i -= (i & (-i)))
-        for (j = k; j; j -= (j & (-j)))
-            res += fw_tree[i].query(j);
-    return res;
-}
-void update(int i, int j, int w)
-{
-    int k = j;
-    for (; i <= MAX; i += (i & (-i)))
-        for (j = k; j <= MAX; j += (j & (-j)))
-            fw_tree[i].add(j, w);
-}
-uint64_t getsum(int l, int r, int d, int u)
-{
-    return getpfs(r, u) + getpfs(l, d) - getpfs(l, u) - getpfs(r, d);
-}
 void init()
 {
 }
@@ -83,13 +16,82 @@ struct Query
 {
     int t, l, r, d, u, x, y, w;
 };
+long long qres[200000];
+struct Fenwick
+{
+    vector<long long> bit;
+    Fenwick(int n) : bit(n + 1, 0)
+    {
+    }
+    void update(int id, int val)
+    {
+        for (; id < bit.size(); id += id & (-id))
+            bit[id] += val;
+    }
+    long long query(int id)
+    {
+        long long sum = 0;
+        for (; id > 0; id -= id & (-id))
+            sum += bit[id];
+        return sum;
+    }
+};
+Fenwick BIT(200000);
+struct Event
+{
+    int a, b, id, type;
+};
+vector<Event> events;
+vector<Query> queries;
+void DNC(int l, int r)
+{
+    if (r - l > 16)
+    {
+        int mid = (l + r) >> 1;
+        // calculate the contribution
+        vector<Event> updl, qryr;
+        for (int i = l; i <= mid; i++)
+            if (events[i].id == -1)
+                updl.push_back(events[i]);
+        for (int i = mid + 1; i <= r; i++)
+            if (events[i].id != -1)
+                qryr.push_back(events[i]);
+        sort(updl.begin(), updl.end(), [](Event &a, Event &b) { return a.a < b.a; });
+        sort(qryr.begin(), qryr.end(), [](Event &a, Event &b) { return a.a < b.a; });
+        int uid = 0;
+        for (int i = 0; i < qryr.size(); i++)
+        {
+            while (uid < updl.size() && updl[uid].a <= qryr[i].a)
+            {
+                BIT.update(updl[uid].b, updl[uid].type);
+                uid++;
+            }
+            qres[qryr[i].id] += qryr[i].type * BIT.query(qryr[i].b);
+        }
+        for (int i = 0; i < uid; i++)
+            BIT.update(updl[i].b, -updl[i].type);
+        DNC(l, mid);
+        DNC(mid + 1, r);
+    }
+    else if (l < r)
+    {
+        for (int i = l; i <= r; i++)
+            if (events[i].id != -1)
+            {
+                long long r = 0;
+                for (int j = l; j < i; j++)
+                    if (events[j].a <= events[i].a && events[j].b <= events[i].b && events[j].id == -1)
+                        r += events[j].type;
+                qres[events[i].id] += events[i].type * r;
+            }
+    }
+}
 void Yoshi()
 {
     int n, q;
     cin >> n >> q;
     vector<int> xv, yv;
-    vector<Query> queries;
-    while (n--)
+    for (int i = 0; i < n; i++)
     {
         int x, y, w;
         cin >> x >> y >> w;
@@ -97,7 +99,7 @@ void Yoshi()
         xv.push_back(x);
         yv.push_back(y);
     }
-    while (q--)
+    for (int i = 0; i < q; i++)
     {
         int t;
         cin >> t;
@@ -120,18 +122,34 @@ void Yoshi()
     sort(yv.begin(), yv.end());
     auto itx = unique(xv.begin(), xv.end());
     auto ity = unique(yv.begin(), yv.end());
-    for (auto &qu : queries)
+    for (int i = 0; i < n + q; i++)
     {
+        auto &qu = queries[i];
         if (qu.t)
-            cout << getsum(lower_bound(xv.begin(), itx, qu.l) - xv.begin(),
-                           lower_bound(xv.begin(), itx, qu.r) - xv.begin(),
-                           lower_bound(yv.begin(), ity, qu.d) - yv.begin(),
-                           lower_bound(yv.begin(), ity, qu.u) - yv.begin())
-                 << endl;
+        {
+            int id1 = lower_bound(xv.begin(), itx, qu.l) - xv.begin();
+            int id2 = lower_bound(xv.begin(), itx, qu.r) - xv.begin();
+            int id3 = lower_bound(yv.begin(), ity, qu.d) - yv.begin();
+            int id4 = lower_bound(yv.begin(), ity, qu.u) - yv.begin();
+            events.push_back({id2, id4, i, 1});
+            events.push_back({id1, id3, i, 1});
+            events.push_back({id1, id4, i, -1});
+            events.push_back({id2, id3, i, -1});
+        }
         else
-            update(upper_bound(xv.begin(), itx, qu.x) - xv.begin(), upper_bound(yv.begin(), ity, qu.y) - yv.begin(),
-                   qu.w);
+        {
+            int id5 = upper_bound(xv.begin(), itx, qu.x) - xv.begin();
+            int id6 = upper_bound(yv.begin(), ity, qu.y) - yv.begin();
+            events.push_back({id5, id6, -1, qu.w});
+        }
     }
+    chrono::high_resolution_clock Clock;
+    auto t1 = Clock.now();
+    DNC(0, events.size() - 1);
+    cerr << "DNC time: " << chrono::duration_cast<chrono::milliseconds>(Clock.now() - t1).count() << "ms" << endl;
+    for (int i = 0; i < n + q; i++)
+        if (queries[i].t)
+            cout << qres[i] << endl;
 }
 signed main()
 {
