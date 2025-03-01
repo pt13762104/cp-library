c2980544c1f39468fbffc1fa1f6d377fdb7d02e3
Yoshi_likes_e4 <104140648+pt13762104@users.noreply.github.com>
2025-03-01 19:01:17 +0700

    AVX2 optimized input

--- yoshi_likes_e4/matmul_mod2.test.cpp
+++ yoshi_likes_e4/matmul_mod2.test.cpp
@@ -1,12 +1,11 @@
-// @brief Matrix multiplication (mod 2)
-#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product_mod_2"
 #pragma GCC optimize("O3,unroll-loops")
 #include <chrono>
-#include <cstring>
+#include <immintrin.h>
 #include <iostream>
 #include <stdio.h>
-#define getchar getchar_unlocked
-#define putchar putchar_unlocked
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 using namespace std;
 #define endl '\n'
 #define debug(x) cerr << #x << " = " << x << endl;
@@ -14,42 +13,84 @@ alignas(32) long long a[4096][64];
 alignas(32) long long b[4096][64];
 alignas(32) long long c[4096][64];
 alignas(32) long long TMP[256][64];
-int rd()
+const __m256i add = {0x4f4f4f4f4f4f4f4fLL, 0x4f4f4f4f4f4f4f4fLL, 0x4f4f4f4f4f4f4f4fLL, 0x4f4f4f4f4f4f4f4fLL};
+uint32_t read_32_bit_from_str(const char *str)
 {
-    int k = 0, f = 1;
-    char s = getchar();
-    while (s < '0' || s > '9')
-    {
-        if (s == '-')
-            f = 0;
-        s = getchar();
-    }
-    while (s >= '0' && s <= '9')
-    {
-        k = (k << 1) + (k << 3) + (s ^ '0');
-        s = getchar();
-    }
-    return f ? k : -k;
+    __m256i tmp = _mm256_loadu_si256((__m256i *)str);
+    return _mm256_movemask_epi8(tmp + add);
 }
-bool rd_bool()
+char *buf1;
+inline namespace Input
 {
-    char c = getchar();
-    while ((c != '0') && (c != '1'))
-        c = getchar();
-    return c ^ '0';
+int pos;
+char next_char()
+{
+    return buf1[pos++];
 }
+int read_int()
+{
+    int x;
+    char ch;
+    int sgn = 1;
+    while (!isdigit(ch = next_char()))
+    {
+        if (ch == '-')
+        {
+            sgn *= -1;
+        }
+    }
+    x = ch - '0';
+    while (isdigit(ch = next_char()))
+    {
+        x = x * 10 + (ch - '0');
+    }
+    return x * sgn;
+}
+}; // namespace Input
 int main()
 {
+    struct stat fs;
+    fstat(0, &fs);
+    buf1 = (char *)mmap(NULL, fs.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
+    if (MAP_FAILED == buf1)
+    {
+        fprintf(stderr, "mmap(): error '%m' (%d)\n", errno);
+        return 1;
+    }
     chrono::high_resolution_clock Clock;
     auto t1 = Clock.now();
     int n, m, k;
-    n = rd(), m = rd(), k = rd();
+    n = read_int(), m = read_int(), k = read_int();
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            a[i][j >> 6] |= rd_bool() * (1LL << (j & 63));
+    {
+        for (int j = 0; j < m / 32; j++)
+        {
+            a[i][j >> 1] |= (uint64_t)read_32_bit_from_str(buf1 + pos) << ((j & 1) * 32);
+            pos += 32;
+        }
+        int c = (m / 32) * 32;
+        for (int j = c; j < m; j++)
+        {
+            a[i][j >> 6] |= uint64_t(buf1[pos] - '0') << (j & 63);
+            pos++;
+        }
+        pos++;
+    }
     for (int i = 0; i < m; i++)
-        for (int j = 0; j < k; j++)
-            b[i][j >> 6] |= rd_bool() * (1LL << (j & 63));
+    {
+        for (int j = 0; j < k / 32; j++)
+        {
+            b[i][j >> 1] |= (uint64_t)read_32_bit_from_str(buf1 + pos) << ((j & 1) * 32);
+            pos += 32;
+        }
+        int c = (k / 32) * 32;
+        for (int j = c; j < k; j++)
+        {
+            b[i][j >> 6] |= uint64_t(buf1[pos] - '0') << (j & 63);
+            pos++;
+        }
+        pos++;
+    }
     cerr << "Input time: " << chrono::duration_cast<chrono::milliseconds>(Clock.now() - t1).count() << " ms" << endl;
     t1 = Clock.now();
     for (int j = 0; j < 4096; j += 8)
@@ -69,11 +110,13 @@ int main()
     }
     cerr << "Compute time: " << chrono::duration_cast<chrono::milliseconds>(Clock.now() - t1).count() << " ms" << endl;
     t1 = Clock.now();
+    char buf[k + 1];
+    buf[k] = '\n';
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < k; j++)
-            putchar((((c[i][j >> 6]) >> (j & 63)) & 1) + '0');
-        putchar('\n');
+            buf[j] = ((c[i][j >> 6] >> (j & 63)) & 1) + '0';
+        write(1, buf, k + 1);
     }
     cerr << "Output time: " << chrono::duration_cast<chrono::milliseconds>(Clock.now() - t1).count() << " ms" << endl;
 }
