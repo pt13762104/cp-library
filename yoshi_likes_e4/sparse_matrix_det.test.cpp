c0e018b4b9869b465d40d0c8b61b2836441cc7c1
Yoshi_likes_e4 <104140648+pt13762104@users.noreply.github.com>
2025-07-08 22:05:05 +0700

    Update sparse_matrix_det.test.cpp

--- yoshi_likes_e4/sparse_matrix_det.test.cpp
+++ yoshi_likes_e4/sparse_matrix_det.test.cpp
@@ -1,7 +1,5 @@
-// @brief Determinant of Sparse Matrix
+#pragma GCC optimize("O3,unroll-loops")
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 #ifndef yoshi_likes_e4
 #define endl '\n'
@@ -10,215 +8,98 @@ using namespace std;
 #define problem ""
 #define multitest 0
 #define debug(x) cerr << #x << " = " << x << endl;
-struct chash
+constexpr int MOD = 998244353;
+int64_t inv_(int64_t x)
 {
-    uint64_t operator()(uint64_t x) const noexcept
+    int64_t v = MOD;
+    int64_t x1 = 1, x2 = 0;
+    while (x != 1)
     {
-        // splitmix64
-        uint64_t z = (x += 0x9e3779b97f4a7c15);
-        z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-        z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-        return z ^ (z >> 31);
+        int64_t q = v / x;
+        int64_t r = v - q * x, X = x2 - q * x1;
+        v = x;
+        x = r;
+        x2 = x1;
+        x1 = X;
     }
+    return (x1 % MOD) + (x1 < 0) * MOD;
+}
+struct Modint
+{
+    uint32_t R;
+    Modint() {}
+    Modint(int c) { R = c; }
+    Modint add(Modint c) { return min(R + c.R, R + c.R - MOD); }
+    Modint sub(Modint c) { return min(R - c.R, R - c.R + MOD); }
+    Modint mul(Modint c) { return (size_t)R * c.R % MOD; }
+    Modint inv() { return inv_(R); }
 };
-const int MOD = 998244353;
-#define int long long
-int bpow(int n, int k)
-{
-    int res = 1;
-    while (k)
-    {
-        if (k & 1)
-        {
-            res *= n;
-            res %= MOD;
-        }
-        n *= n;
-        n %= MOD;
-        k >>= 1;
-    }
-    return res;
-}
-int inv(int i)
-{
-    return bpow(i, MOD - 2);
-}
-int us(int a, int b)
-{
-    int r = (a - b) % MOD;
-    return r + (r < 0 ? MOD : 0);
-}
-#undef int
+typedef array<unsigned long long, 47> AsArray;
 const int B = 64;
-struct PredProblem
+bool getbit(AsArray &x, int b) { return (x[b / B] >> (b % B)) & 1; }
+int stor[9000000];
+int cnt[3000];
+Modint *RAW[3000];
+bitset<3000> Nz[3000];
+int64_t __ref()
 {
-    int n, h, sz;
-    vector<vector<uint64_t>> tree;
-    void init(int old_n)
-    {
-        n = old_n;
-        while (n > 1)
-        {
-            // n = ceil(n/B)
-            tree.push_back(vector<uint64_t>(n = (n + B - 1) / B));
-        }
-        n = old_n, h = tree.size();
-    };
-    // remember: tree[i][k][j] means that at the jth subtree of tree[i][k] has sum > 0
-    // This is copied from noimi
-    void insert(int i)
-    {
-        if (get(i))
-            return;
-        sz++;
-        // log_B(n)
-        for (auto &v : tree)
-        {
-            v[i / B] |= 1ULL << (i % B);
-            i /= B;
-        }
-    };
-    void erase(int i)
-    {
-        if (!get(i))
-            return;
-        sz--;
-        // log_B(n)
-        for (auto &v : tree)
-        {
-            v[i / B] &= ~(1ULL << (i % B));
-            i /= B;
-            // do not erase the above subtrees if v[i] is not empty
-            if (v[i])
-                break;
-        }
-    };
-    int size() { return sz; }
-    bool get(int i)
-    {
-        return (tree[0][i / B] >> (i % B)) & 1;
-    };
-    int next(int i)
-    {
-        // log_B(n)
-        for (int j = 0; j < h; j++)
-        {
-            // get the position in the current subtree
-            if (i / B >= tree[j].size())
-                break;
-            int pos = i % B;
-            uint64_t c = tree[j][i / B];
-            // mask off the first pos bits
-            c &= ~((1ULL << pos) - 1);
-            i /= B;
-            if (!c)
-            {
-                // jump to the next tree
-                i++;
-            }
-            else
-            {
-                // if we find the first bit set, try to go deeper
-                int i2 = i * B + __builtin_ctzll(c);
-                for (int k = j - 1; k >= 0; k--)
-                {
-                    i2 *= B;
-                    i2 += __builtin_ctzll(tree[k][i2 / B]);
-                }
-                return i2;
-            }
-        }
-        return n;
-    };
-    int prev(int i)
-    {
-        // log_B(n)
-        for (int j = 0; j < h; j++)
-        {
-            // get the position in the current subtree
-            int pos = i % B;
-            uint64_t c = tree[j][i / B];
-            // keep only the first pos bits
-            if (pos != B - 1)
-                c %= (1ULL << (pos + 1));
-            i /= B;
-            if (!c)
-            {
-                // jump to the PREVIOUS tree
-                if (!i)
-                    break;
-                i--;
-            }
-            else
-            {
-                // if we find the last bit set, try to go deeper
-                int i2 = i * B + 63 - __builtin_clzll(c);
-                for (int k = j - 1; k >= 0; k--)
-                {
-                    i2 *= B;
-                    i2 += 63 - __builtin_clzll(tree[k][i2 / B]);
-                }
-                return i2;
-            }
-        }
-        return -1;
-    };
-};
-int i_stack[3000];
-int64_t __ref(vector<vector<int>> &x)
-{
-    vector<PredProblem *> Nz(x.size());
-    for (int i = 0; i < x.size(); i++)
-    {
-        Nz[i] = new PredProblem();
-        Nz[i]->init(x.size() + 5);
-    }
-    for (int i = 0; i < x.size(); i++)
-        for (int idx = 0; idx < x.size(); idx++)
-            if (x[i][idx])
-                Nz[i]->insert(idx);
     int h = 0, k = 0;
     int64_t sign = 1;
-    while (h < x.size() && k < x.size())
+    int i_stack[3000];
+    while (h < 3000 && k < 3000)
     {
-        int cur_last = 1e9, idx = x.size();
-        for (int i = h; i < x.size(); i++)
+        int cur_last = 1e9, idx = 3000;
+        for (int i = h; i < 3000; i++)
         {
-            if (x[i][k])
-                if (Nz[i]->size() < cur_last)
+            AsArray &ar2 = *reinterpret_cast<AsArray *>(&Nz[i]);
+            if (getbit(ar2, k))
+                if (cnt[i] < cur_last)
                 {
-                    cur_last = Nz[i]->size();
+                    cur_last = cnt[i];
                     idx = i;
                 }
         }
-        if (idx != x.size())
+        if (idx != 3000)
         {
-            swap(x[h], x[idx]);
+            swap(RAW[h], RAW[idx]);
             swap(Nz[h], Nz[idx]);
-            const int64_t ixhk = inv(x[h][k]);
+            swap(cnt[h], cnt[idx]);
+            const Modint ixhk = RAW[h][k].inv();
             if (idx != h)
             {
                 sign *= (MOD - 1);
                 sign %= MOD;
             }
             int i_ptr = 0;
-            int j = Nz[h]->next(0);
-            while (j < x.size())
+            AsArray &ar2 = *reinterpret_cast<AsArray *>(&Nz[h]);
+            int idx = 0;
+            for (auto x : ar2)
             {
-                i_stack[i_ptr++] = j;
-                j = Nz[h]->next(j + 1);
-            }
-            for (int i = h + 1; i < x.size(); i++)
-            {
-                int64_t coeff = (ixhk * x[i][k]) % MOD;
-                for (int k = 0; k < i_ptr; k++)
+                while (x)
                 {
-                    auto j = i_stack[k];
-                    x[i][j] = us(x[i][j], coeff * x[h][j]);
-                    if (!x[i][j])
-                        Nz[i]->erase(j);
-                    if (x[i][j])
-                        Nz[i]->insert(j);
+                    int idx2 = __builtin_ctzll(x) + idx * 64;
+                    if (idx2 < 3000)
+                        i_stack[i_ptr++] = idx2;
+                    x -= (x & -x);
+                }
+                idx++;
+            }
+            for (int i = h + 1; i < 3000; i++)
+            {
+                if (RAW[i][k].R)
+                {
+                    AsArray &ar = *reinterpret_cast<AsArray *>(&Nz[i]);
+                    Modint coeff = RAW[i][k].mul(ixhk);
+                    for (int k = 0; k < i_ptr; k++)
+                    {
+                        auto j = i_stack[k];
+                        cnt[i] -= getbit(ar, j);
+                        RAW[i][j] = RAW[i][j].sub(coeff.mul(RAW[h][j]));
+                        if (RAW[i][j].R) [[likely]]
+                            Nz[i].set(j, 1), cnt[i]++;
+                        else
+                            Nz[i].set(j, 0);
+                    }
                 }
             }
             h++;
@@ -227,60 +108,42 @@ int64_t __ref(vector<vector<int>> &x)
     }
     return sign;
 }
-int rd()
-{
-    int k = 0, f = 1;
-    char s = getchar();
-    while (s < '0' || s > '9')
-    {
-        if (s == '-')
-            f = 0;
-        s = getchar();
-    }
-    while (s >= '0' && s <= '9')
-    {
-        k = (k << 1) + (k << 3) + (s ^ '0');
-        s = getchar();
-    }
-    return f ? k : -k;
-}
-void init()
-{
-}
+void init() {}
 void Yoshi()
 {
-    int N = rd(), K = rd();
-    vector<vector<int>> raw(N, vector<int>(N));
+    int N, K;
+    cin >> N >> K;
+    for (int i = 0; i < 3000; i++)
+        RAW[i] = (Modint *)(stor + 3000 * i);
     while (K--)
     {
-        int a = rd(), b = rd(), x = rd();
-        raw[a][b] = x;
+        int a, b, c;
+        cin >> a >> b >> c;
+        RAW[a][b] = c;
+        Nz[a].set(b, 1);
+        cnt[a]++;
     }
     vector<pair<int, int>> st;
-    for (int i = 0; i < raw.size(); i++)
-    {
-        int non_zero = 0;
-        for (auto &k : raw[i])
-            non_zero += (bool)k;
-        st.push_back({non_zero, i});
-    }
+    for (int i = 0; i < N; i++)
+        st.push_back({cnt[i], i});
     int64_t sgn = 1;
-    for (int i = 0; i < raw.size(); i++)
+    for (int i = 0; i < N; i++)
     {
         int ptr = min_element(st.begin() + i, st.end()) - st.begin();
         if (ptr != i)
         {
             sgn *= MOD - 1, sgn %= MOD;
-            swap(raw[st[i].second], raw[st[ptr].second]);
+            swap(cnt[st[i].second], cnt[st[ptr].second]);
+            swap(Nz[st[i].second], Nz[st[ptr].second]);
+            swap(RAW[st[i].second], RAW[st[ptr].second]);
             swap(st[i], st[ptr]);
         }
     }
-    // gaussian elimination
-    int64_t sign = __ref(raw);
+    int64_t sign = __ref();
     int64_t determinant = (sign * sgn) % MOD;
     for (int i = 0; i < N; i++)
     {
-        determinant *= raw[i][i];
+        determinant *= RAW[i][i].R;
         determinant %= MOD;
     }
     cout << determinant << endl;
@@ -290,7 +153,6 @@ signed main()
 #ifndef yoshi_likes_e4
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
     if (fopen(problem ".inp", "r"))
     {
         freopen(problem ".inp", "r", stdin);
